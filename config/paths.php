045d2f16c66358575a5e076e9407986c6a6e7fbf
Sisikelelwe Maqabangqa <sakes97@gmail.com>
2019-08-15 12:26:32 +0200

    autoload()

--- config/paths.php
+++ config/paths.php
@@ -1,3 +1,5 @@
 <?php 
 //for absolute/relative path
 define('URL','http://localhost:8080/myphp/hikeshare/');
+
+define('LIBS', 'libs/');
--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
 
-require 'libs/Bootstrap.php';
-require 'libs/Controller.php';
-require 'libs/Model.php';
-require 'libs/View.php';
-require 'libs/Database.php';
-require 'libs/Session.php';
+// require 'libs/Bootstrap.php';
+// require 'libs/Controller.php';
+// require 'libs/Model.php';
+// require 'libs/View.php';
+// require 'libs/Database.php';
+// require 'libs/Session.php';
 
 
 require 'config/paths.php';
 require 'config/database.php';
+
+
+function __autoload($class)
+{
+    require LIBS.$class.'php';
+}
+
 
 $app = new Bootstrap();

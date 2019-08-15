06b23dbc45bf59cf2d8a2d9794b99b3df4dffef8
Sisikelelwe Maqabangqa <sakes97@gmail.com>
2019-08-15 13:11:45 +0200

    index update

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 
-// require 'libs/Bootstrap.php';
-// require 'libs/Controller.php';
-// require 'libs/Model.php';
-// require 'libs/View.php';
-// require 'libs/Database.php';
-// require 'libs/Session.php';
-
-
-// require 'config/paths.php';
-// require 'config/database.php';
-// require 'config/constants.php';
-
 require 'config.php';
 
 function __autoload($class)

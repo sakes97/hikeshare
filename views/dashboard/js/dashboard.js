474b62ca08748e5cf9de34098380c604bd8964ba
Sisikelelwe Maqabangqa <sakes97@gmail.com>
2019-09-16 00:48:53 +0200

    car image preview

--- views/dashboard/js/dashboard.js
+++ views/dashboard/js/dashboard.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //datetimepicker - day/month/year
     $('.datepicker').datetimepicker({
         icons: {
             time: "fa fa-clock-o",
@@ -15,6 +16,23 @@ $(document).ready(function () {
         minDate: new Date()
     });
 
+    //datetimepicker - year
+    $('.datepicker_year').datetimepicker({
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            up: "fa fa-chevron-up",
+            down: "fa fa-chevron-down",
+            previous: 'fa fa-chevron-left',
+            next: 'fa fa-chevron-right',
+            today: 'fa fa-screenshot',
+            clear: 'fa fa-trash',
+            close: 'fa fa-remove'
+        },
+        format: 'YYYY',
+    });
+
+    //datetimepicker - time
     $('.timepicker').datetimepicker({
         icons: {
             time: "fa fa-clock-o",
@@ -29,7 +47,37 @@ $(document).ready(function () {
         },
         format: 'LT'
     });
-    
+
+
+    $("#inputGroupFile01").change(function(event){
+        RecurFadeIn();
+        readURL(this);
+    });
+
+    $("#inputGroupFile01").on('click', function(event){
+        RecurFadeIn();
+    });
+
+    function readURL(input){
+        if(input.files && input.files[0]){
+            var reader = new FileReader();
+            var filename = $("#inputGroupFile01").val();
+            filename = filename.substring(filename.lastIndexOf('\\')+1);
+            reader.onload = function(e) {
+                debugger;
+                $("#image-upload").attr('src', e.target.result);
+                $("#image-upload").hide();
+                $("#image-upload").fadeIn(500);
+                $(".custom-file-label").text(filename);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    function RecurFadeIn(){
+        console.log('ran');
+    }
+
 });
 
 function placeSearch(){
--- views/dashboard/profile.php
+++ views/dashboard/profile.php
@@ -12,8 +12,8 @@ if(!isset($_GET['profile_view'])){
     <h2>Profile</h2>
     <!-- infromation form -->
     <div class="row">
-      <div class="col-4">
-        <select name="profile_view" class="d-inline-block form-control form-control-sm d-inline-block"
+      <div class="col-xs-4 col-md-3">
+        <select name="profile_view" class="form-control form-control-sm"
         id="profile_view" onchange="pageview(this.value)">
           <option value="0" <?php if($option == 0) echo "selected"; ?> >Profile Picture</option>
           <option value="1" <?php if($option == 1) echo "selected"; ?> >Preferences</option>
@@ -201,59 +201,79 @@ if(!isset($_GET['profile_view'])){
       ?>
       <!-- car details -->
       <div class="row">
-      <div class="col-12">
-        <h6 class="mt-2 pb-0 text-center">Car Details</h6>
-        <form>
-          <!-- number plate/registration number -->
-          <div class="form-row">
-            <div class="form-group col-sm-6 col-md-4 p-3">
-              <label for="number_plate">Number Plate</label>
-              <input type="text" class="form-control" id="number_plate" placeholder="number_plate">
+        <div class="col-12">
+          <h6 class="mt-2 pb-0 text-center">Car Details</h6>
+          <form>
+            <!-- car image -->
+            <div class="form-row">
+              <div class="form-group col-md-4 p-3 mr-auto ml-auto text-center">
+                <label for="image-upload">Upload Car Image</label>
+                <div id="image_contain">
+                  <img id="image-upload" align="middle" class="img-square"
+                  src="<?php echo URL; ?>public/images/hikeshare-logo.png" alt="Car Image"
+                  title=''/>
+                </div>
+              </div>
             </div>
-            <div class="form-group col-sm-6 col-md-4 p-3">
-                <label for="registration_number">Registration Number</label>
-                <input type="text" class="form-control" id="registration_number" placeholder="registration_number">
+            <div class="form-row">
+              <div class="form-group col-sm-8 col-md-4 mr-auto ml-auto">
+                <input type="file" id="inputGroupFile01" class="form-control imgInp custom-file-input" 
+                aria-describedby="inputGroupFile01">
+                <label class="custom-file-label" for="inputGroupFile01">Choose Image</label>
+              </div>
             </div>
-          </div>
-          <!-- end number plate/registration number -->
+            <!-- end car image -->
 
-          <!-- model/make -->
-          <div class="form-row">
-            <div class="form-group col-sm-6 col-md-4 p-3">
-              <label for="model">Model</label>
-              <input type="text" class="form-control" name="model" id="model">
+            <!-- registration number -->
+            <div class="form-row">
+              <div class="form-group col-sm-6 col-md-4 p-3 ">
+                  <label for="registration_number">Registration Number</label>
+                  <input type="text" class="form-control" id="registration_number" placeholder="Registration Number">
+              </div>
             </div>
-            <div class="form-group col-sm-6 col-md-4 p-3">
-              <label for="make">Make</label>
-              <input name="make" id="make" class="form-control" type="text">
-            </div>
-          </div>
-          <!-- end model/make -->
+            <!-- end registration number -->
 
-          <!-- number of seats -->
-          <div class="form-row">
-            <div class="form-group col-sm-6 col-md-4 p-3">
-              <label for="number_of_seats">Number Of Seats</label>
-              <input class="form-control" type="number" min="2" max="16" name="number_of_seats" id="number_of_seats">
+            <!-- make/model/model-year -->
+            <div class="form-row">
+              <div class="form-group col-sm-12 col-md-4 p-3">
+                <label for="make">Make</label>
+                <input name="make" id="make" class="form-control" type="text" placeholder="Make">
+              </div>
+              <div class="form-group col-sm-12 col-md-4 p-3">
+                <label for="model">Model</label>
+                <input type="text" class="form-control" name="model" id="model" placeholder="Model">
+              </div>
+              <div class="form-group col-sm-12 col-md-4 p-3">
+                <label for="model_year" class="label-control">Model Year</label>
+                <input type="text" class="form-control datepicker_year" name="model_year" id="model_year" placeholder="Model Year">
+              </div>
             </div>
-          </div>
-          <!-- end number of seats -->
+            <!-- end make/model/model-year -->
 
-          <!-- colour -->
-          <div class="form-row">
-            <div class="form-group col-sm-6 col-md-4 p-3">
-              <label for="colour">Colour</label>
-              <input type="text" class="form-control" name="colour" id="colour" placeholder="Colour">
+            <!-- colour -->
+            <div class="form-row">
+              <div class="form-group col-sm-6 col-md-4 p-3">
+                <label for="colour">Colour</label>
+                <input type="text" class="form-control" name="colour" id="colour" placeholder="Colour">
+              </div>
             </div>
-          </div>
-          <!-- end colour -->
+            <!-- end colour -->
 
-          <!-- button -->
-          <div class="text-center pb-3">
-            <input type="submit" class="btn btn-danger btn-round" value="submit">
-          </div>
-          <!-- end button -->
-        </form>
+            <!-- number of seats -->
+            <div class="form-row">
+              <div class="form-group col-sm-8 col-md-3 p-3">
+                <label for="number_of_seats">Number Of Seats (Excluding Driver Seat)</label>
+                <input class="form-control" type="number" min="1" max="16" name="number_of_seats" id="number_of_seats">
+              </div>
+            </div>
+            <!-- end number of seats -->
+
+            <!-- button -->
+            <div class="text-center pb-3">
+              <input type="submit" class="btn btn-danger btn-round" value="submit">
+            </div>
+            <!-- end button -->
+          </form>
         </div>
       </div>
       <!-- end car details -->

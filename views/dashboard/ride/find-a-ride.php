9ef6722c34e08f380ffe7f30844e4ae1f05fbcec
Sisikelelwe Maqabangqa <sakes97@gmail.com>
2019-10-03 15:44:59 +0200

    .

--- views/dashboard/ride/find-a-ride.php
+++ views/dashboard/ride/find-a-ride.php
@@ -1,5 +1,8 @@
 <div class="section profile-content">
     <div class="container">
-        
+        <!--large screens view-->
+        <div class="row">
+            <h4>Find a ride</h4>
+        </div>
     </div>
 </div>
